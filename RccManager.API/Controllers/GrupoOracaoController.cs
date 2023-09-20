3bbe4a247f99062900ac70ca94e3c74db754a8c9
William Nascimento <william.nascimento2010@gmail.com>
2023-09-20 19:34:59 -0300

    Authorize GrupoOracao

--- RccManager.API/Controllers/GrupoOracaoController.cs
+++ RccManager.API/Controllers/GrupoOracaoController.cs
@@ -1,57 +1,53 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RccManager.Domain.Dtos.GrupoOracao;
-using RccManager.Domain.Dtos.ParoquiaCapela;
 using RccManager.Domain.Interfaces.Services;
-using RccManager.Service.Services;
 
-namespace RccManager.API.Controllers
+namespace RccManager.API.Controllers;
+
+[ApiController]
+[Route("api/v1/grupo-oracao")]
+[Authorize]
+public class GrupoOracaoController : ControllerBase
 {
-    [ApiController]
-    [Route("api/v1/grupo-oracao")]
-    //[Authorize]
-    public class GrupoOracaoController : ControllerBase
+    private readonly IGrupoOracaoService _grupoOracaoService;
+
+    public GrupoOracaoController(IGrupoOracaoService grupoOracaoService)
     {
-        private readonly IGrupoOracaoService _grupoOracaoService;
+        _grupoOracaoService = grupoOracaoService;
+    }
 
-        public GrupoOracaoController(IGrupoOracaoService grupoOracaoService)
-        {
-            _grupoOracaoService = grupoOracaoService;
-        }
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] string search)
+    {
+        string _search = string.Empty;
+        if (search != null)
+            _search = search;
 
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] string search)
-        {
-            string _search = string.Empty;
-            if (search != null)
-                _search = search;
+        var grupoOracoes = await _grupoOracaoService.GetAll(_search);
+        return Ok(grupoOracoes);
+    }
 
-            var grupoOracoes = await _grupoOracaoService.GetAll(_search);
-            return Ok(grupoOracoes);
-        }
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] GrupoOracaoDto grupoOracaoDto)
+    {
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] GrupoOracaoDto grupoOracaoDto)
-        {
+        await _grupoOracaoService.Create(grupoOracaoDto);
+        return Ok(HttpStatusCode.Created);
 
-            await _grupoOracaoService.Create(grupoOracaoDto);
-            return Ok(HttpStatusCode.Created);
+    }
 
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, [FromBody] GrupoOracaoDto grupoOracaoDto)
+    {
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(Guid id, [FromBody] GrupoOracaoDto grupoOracaoDto)
-        {
+        var updatedGrupoOracao = await _grupoOracaoService.Update(grupoOracaoDto, id);
+        if (updatedGrupoOracao == null)
+            return NotFound();
 
-            var updatedGrupoOracao = await _grupoOracaoService.Update(grupoOracaoDto, id);
-            if (updatedGrupoOracao == null)
-                return NotFound();
+        return Ok(HttpStatusCode.NoContent);
 
-            return Ok(HttpStatusCode.NoContent);
-
-        }
     }
 }
 
